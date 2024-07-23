41f8d97599118814879421c7de405752ecf06f3f
mxrsel <marsel14082006@gmail.com>
2024-07-23 14:07:12 +0600

    added fetch for contacts for get data and added to extra reducers

--- src/store/contactSlice.ts
+++ src/store/contactSlice.ts
@@ -1,4 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {AppDispatch} from "../app/store.ts";
+import axiosApi from "../axiosApi.ts";
 
 interface Contact{
     id: string;
@@ -18,10 +20,42 @@ const initialState: ContactState = {
     contacts: [],
     loading: false,
     error: null,
-}
+};
+
+export const fetchContacts = createAsyncThunk<Contact[], undefined, {dispatch: AppDispatch}> ('contacts/fetchContacts', async () => {
+    const contactsResponse = await axiosApi.get('/contacts.json');
+    const contacts = contactsResponse.data;
+
+    let newContacts: Contact[] = [];
+
+    if(contacts) {
+        newContacts = Object.keys(contacts).map((key: string) => {
+            const contact = contacts[key];
+            return {
+                id: key,
+                ...contact,
+            };
+        });
+    }
+
+return newContacts;
+});
 
 const contactSlice = createSlice({
     name: "contact",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchContacts.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchContacts.fulfilled, (state, {payload: contacts}) => {
+                state.loading = false;
+                state.contacts = contacts;
+            })
+            .addCase(fetchContacts.rejected, (state) => {
+                state.loading = false;
+            });
+    }
 })
