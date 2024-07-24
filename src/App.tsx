93e6cd18995db20361ad75f29514411305e99f78
mxrsel <marsel14082006@gmail.com>
2024-07-24 17:32:49 +0600

    added list and form on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import {Route, Routes, NavLink} from 'react-router-dom';
+import ContactList from "./components/Contacts/Contacts.tsx";
+import ContactFormPage from "./pages/ContactFormPage/ContactFormPage.tsx";
+import './App.css'
 
-const App: React.FC = () => {
-    return (
-        <div>
 
-        </div>
-    );
-};
+const App = () => (
+    <div>
+        <nav>
+            <NavLink to="/">Contacts</NavLink>
+            <NavLink to="/add">Add New Contact</NavLink>
+        </nav>
+        <Routes>
+            <Route path="/" element={<ContactList/>}/>
+            <Route path="/add" element={<ContactFormPage/>}/>
+            <Route path="/edit/:id" element={<ContactFormPage/>}/>
+        </Routes>
+    </div>
+);
 
 export default App;
